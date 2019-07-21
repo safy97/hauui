0ba443cf98689eba1ce3a02a3cb4dd999f64de94
Alaa Moaataz <alaa.moaataz@inovaeg.com>
2019-07-21 20:38:12 +0200

    fix reply model

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,11 +1,11 @@
 class reply < ApplicationRecord
 	#associations
 	belongs_to :comment
-	belongs_to :reply
+	belongs_to :user
 
 	#validations
 	validates :content , presence: true
 
-	
+
 	#callbacks
 end
